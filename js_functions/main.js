b11645a18b3f8f67b4f8521a4038e05041be41d3
KoreyLo <koreylo9@gmail.com>
2016-01-20 17:42:40 -0800

    Feature Set 2 & 3

--- js_functions/main.js
+++ js_functions/main.js
@@ -1,3 +1,4 @@
+//Feature Set 1
 function myMessage()
 {
     console.log("My first function!");
@@ -6,4 +7,18 @@ function myMessage()
 function add(x,y){
     var z = x+y;
     console.log(z)
+}
+// Feature Set 2
+
+function add2(num1,num2)
+{
+    var total = num1 + num2;
+    return total;
+}
+var add2result = add2(9,23);
+
+// Feature Set 3
+function cardFlip(param)
+{
+    $(param).hide();
 }
