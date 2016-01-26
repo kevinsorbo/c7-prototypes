34b286b5c59fe324a569068d4b9dc970aa9981a9
KoreyLo <koreylo9@gmail.com>
2016-01-26 15:30:01 -0800

    Feature set 1,2,3

--- js_event_delegation/script.js
+++ js_event_delegation/script.js
@@ -1,10 +1,29 @@
 // Answer the questions here:
 
 // - What do you think is going to happen?
-
+    // I think that when you click button 5 the content will be displayed in the console like the other buttons.
 // - What happened?
-
+    // Nothing was displayed in the console
 // - Why?
-
+   // I think when we used .on it only gave the event to the current buttons that are present.  Any button made after will not receive the event handler.
 //========== Write your code below ===========//
+
+$(document).ready(function(){
+    // Feature Set 1
+    $("#list button").on("click", function()
+    {
+        console.log($(this).text());
+    });
+
+    //Feature Set 2
+    var newButton = $("<li><button style='margin-top: 10px'>Delegated Button#5 Handler</button></li>");
+    $("#list").append(newButton);
+
+
+
+    //Feature Set 3
+    $("#list").on("click","button", function(){
+        console.log($(this).text());
+    });
+});
 
