c89ddcc4a264740bbeee4802eb49bdc20c68ee71
KoreyLo <koreylo9@gmail.com>
2016-02-09 14:39:32 -0800

    Feature Set 1

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -1,5 +1,5 @@
 //Create GLOBAL variable below here on line 2
-
+var global_result;
 
 $(document).ready(function(){
     $('button').click(function(){
@@ -9,7 +9,7 @@ $(document).ready(function(){
             url: 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json',
             success: function(result) {
                 console.log('AJAX Success function called, with the following result:', result);
-
+                global_result = result;
             }
         });
         console.log('End of click function');
