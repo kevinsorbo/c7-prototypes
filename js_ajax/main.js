dabe7517dc957552088edda13f6627a82c3f8320
KoreyLo <koreylo9@gmail.com>
2016-02-09 15:24:08 -0800

    Feature Set 3

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -1,6 +1,8 @@
 //Create GLOBAL variable below here on line 2
 var global_result;
 var first_movie_info;
+var newElement;
+var x;
 $(document).ready(function(){
     $('button').click(function(){
         console.log('click initiated');
@@ -11,6 +13,13 @@ $(document).ready(function(){
                 console.log('AJAX Success function called, with the following result:', result);
                 global_result = result;
                 first_movie_info = global_result.feed.entry[0]["im:image"][2].label;
+
+                for(var i = 0; i < global_result.feed.entry.length; i++){
+                    x = global_result.feed.entry[i]["im:image"][2].label;
+                    newElement = $("<img>").attr("src",x);
+                    //console.log("append");
+                    $("#main").append(newElement);
+                }
             }
         });
         console.log('End of click function');
