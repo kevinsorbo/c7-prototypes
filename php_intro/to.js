6d67ed3f065c99e7b1b8a8a9f5b9af082f622992
Korey Lo <koreylo9@gmail.com>
2016-03-03 11:57:39 -0800

    Done with first 5 exercises

--- php_intro/to.js
+++ php_intro/to.js
@@ -1,22 +1,35 @@
 //EXERCISE 1
-
+var variable1 = "hello";
+var variable2 = 4;
+var variable3 = ["foo","bar"];
 
 
 //EXERCISE 2
-
+var variable1 = 4;
+var variable1 = "hello";
 
 
 //EXERCISE 3
+var numbers = [3,5,16,3,4,8];
+var result = numbers[0];
 
+for(var i = 0; i < numbers.length; i++){
+    result += numbers[i];
+}
+
+console.log("Result =",result);
 
 
 //EXERCISE 4
 
-
+console.log("this is a string" + "this is another string");
 
 //EXERCISE 5
 
-
+var my_float = 3.5;
+var my_int = parseInt(my_float);
+var my_float2 = 5.3;
+var my_int2 = parseInt(my_float2);
 
 //EXERCISE 6
 
