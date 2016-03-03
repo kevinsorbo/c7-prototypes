d7f2cc20965dab6ef5a9170a495fc28f32b56c56
Korey Lo <koreylo9@gmail.com>
2016-03-03 12:27:08 -0800

    finished 6-10 exercises

--- php_intro/to.js
+++ php_intro/to.js
@@ -33,20 +33,80 @@ var my_int2 = parseInt(my_float2);
 
 //EXERCISE 6
 
+var my_var1 = "Hello";
+
+if(my_var2){
+    console.log(my_var2);
+}
+else{
+    console.log(my_var1);
+}
 
 
 //EXERCISE 7
 
+var check_var = "What time is it?";
+switch (check_var){
+    case "hello":
+        console.log("greetings");
+        break;
+    case "bye":
+        console.log("bye");
+        break;
+    default:
+        console.log("It\'s party time!");
+        break;
+}
 
 
 //EXERCISE 8
-
+ var student = {
+     name:"skippy",
+     class:"English",
+     grade:75
+ };
 
 
 //EXERCISE 9
+ var student = {};
 
+student.name = "skippy";
+student.class = "English";
+student.grade = 75;
 
 
 //EXERCISE 10
 
+var num_array = [35, 2, 14, 56, 65, 52];
 
+function findGreatestNumAndIndex(direction){
+    var greatest = null;
+    var greatestIndex = null;
+    var i;
+    var increment;
+    var endLoop;
+
+    if(direction >= 0){
+        i = 0;
+        increment = 1;
+        endLoop = num_array.length;
+    }
+    else{
+        i = num_array.length;
+        increment = -1;
+        endLoop = 0;
+    }
+
+    while(i != endLoop){
+        if(num_array[i] > greatest){
+            greatest = num_array[i];
+            greatestIndex = i;
+        }
+        i += increment;
+    }
+
+    return ("greatest: " + greatest + ", greatestIndex: " + greatestIndex);
+}
+
+
+findGreatestNumAndIndex(1);
