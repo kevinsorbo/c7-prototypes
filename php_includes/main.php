3f09b04eacec380f8e64a4b660bca2ab197b7f53
Korey Lo <koreylo9@gmail.com>
2016-03-04 15:07:09 -0800

    feature set 3

---
+++ php_includes/main.php
@@ -0,0 +1,5 @@
+<main id="main_content">
+    Hello, my name is Korey Lo and I'm a web developer.  I'm currently attending LearningFuze so that I can
+    gain the skills to become an excellent programmer.
+    <p><a target="_blank" href="http://srv-live-02.lazada.com.ph/cms/spinbasket/brands-banner/06192014/Lazada-Brand-Banner-NIKE_957x300.jpg">Nike</a></p>
+</main>
