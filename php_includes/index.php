898847b32588db2fa6b3d7e1c42d9753b51a75b8
Korey Lo <koreylo9@gmail.com>
2016-03-04 14:56:23 -0800

    feature set 2

--- php_includes/index.php
+++ php_includes/index.php
@@ -5,3 +5,12 @@ include('includes/data.php');
 $result = $a * $b;
 print("<br>Result is $result");
 ?>
+
+<?php
+$c = 2;
+include('includes/data2.php');
+include_once('includes/data2.php');
+include('includes/data2.php');
+$result2 = $c;
+print("<br>Result 2 = $result2");
+?>
