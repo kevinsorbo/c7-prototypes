221c600ee0649791c9b003eedc6b617e0dbdc6b3
Korey Lo <koreylo9@gmail.com>
2016-03-09 15:02:04 -0800

    working on regex still

--- php_sessions/session_reader.php
+++ php_sessions/session_reader.php
@@ -1,7 +1,12 @@
 <!-- Session Reader -->
 <?php
     session_start();
-    foreach($_GET as $inputLabel => $inputValue){
-        print("Label: $inputLabel; Value: $inputValue <br>");
- }
+//    foreach($_GET as $inputLabel => $inputValue){
+//        print("Label: $inputLabel; Value: $inputValue <br>");
+    header('location: session_setter.php');
+
+    if(!preg_match('^[1-9]$',$_GET['name'])){
+
+    }
+// }
  ?>
