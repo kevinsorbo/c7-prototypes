f04b8a3096b6fb7c7ea0544a662c3cc2a883b88c
Korey Lo <koreylo9@gmail.com>
2016-03-09 10:47:11 -0800

    added comments and a little styling

--- ang_services_intro/script.js
+++ ang_services_intro/script.js
@@ -4,10 +4,15 @@ var tunes = angular.module('myTunes',[]);
 tunes.controller('tunesController',function($scope,$http,$log){
 
     var self = this;
+    // constructURL method
+    // input: userInput
+    // purpose: this function takes the userInput and creates a url link
     this.constructURL = function(userInput){
         $scope.url = "https://itunes.apple.com/search?term="+userInput+"&callback=JSON_CALLBACK";
     };
-
+    // getData method
+    // input: none
+    // purpose: this function uses http to make call to get data about user specified artist from itunes
     this.getData = function(){
         $http({
             url: $scope.url,
@@ -15,7 +20,7 @@ tunes.controller('tunesController',function($scope,$http,$log){
         })
         .then(function(response){
             $log.log(response);
-            self.data = response.data.results;
+            self.data = response.data.results; // stores data into self.data
         },
         function(response){
             $log.log("failure");
