34a6ea6cb6c8d50041223be6b07abd22164b0be2
Korey Lo <koreylo9@gmail.com>
2016-03-08 18:00:31 -0800

    feature set 3 & 4

--- php_directory_operations/carousel.php
+++ php_directory_operations/carousel.php
@@ -12,56 +12,89 @@
 
     <script>
         var imageArray = [];
+        var imageLocation = [];
+        var imgArrayIndex = 0;
+        var stillAnimating = false;
         function loadFiles(){
             $.ajax({
                 url:"get_images.php",
                 dataType:"JSON",
                 success: function(response){
-                    var imgContainer = $("<div>",{
-                        id:"imgContainer",
-                        width:"50vw"
-                    });
-                    $("body").append(imgContainer);
-                    for(var i =0; i < response.files.length; i++){
+                    imageArray = response.files;
+                    for(var i = 0; i < imageArray.length; i++){
                         var img = $("<img>",{
-                            src:response.files[i],
-                            width:"100%",
-                            style:"position:absolute",
-                            left:"0px",
-                            top:"0px",
-                            class:"img"+i
+                            src: imageArray[i],
+                            style:"position:absolute; width:100%; height:100%; right:100%; top:0px"
                         });
-                        imgContainer.append(img);
-                        imageArray.push(response.files[i]);
+                        $('#imgContainer').append(img);
+                        imageLocation.push(img);
                     }
-
-
+                    imageLocation[0].css("right","0");
                 }
             });
+            console.log("hello");
         }
 
-        function prevImg() {
-
-        }
-
-        function nextImg(){
-            console.log("next");
+        function moveLeftRight(direction){
+            if(stillAnimating){
+                return;
+            }
+            stillAnimating = true;
+            var next_position = imgArrayIndex + direction;
+            if(next_position == -1){
+                next_position = 6;
+            }
+            if(next_position > 6){
+                next_position = 0;
+            }
+            var currentImg = imageLocation[imgArrayIndex];
+            var nextImg = imageLocation[next_position];
+            nextImg.css('right',100*direction*-1+"%");
+            nextImg.animate({'right':0},1000,function(){
+                stillAnimating = false;
+            });
+            currentImg.animate({'right':100*direction+"%"},1000);
+            imgArrayIndex = next_position;
         }
 
         $(document).ready(function(){
             loadFiles();
+
             $("#previous").on("click",function(){
-                prevImg();
+                moveLeftRight(-1);
             });
             $("#next").on("click", function(){
-                nextImg();
+                moveLeftRight(1);
             });
         });
 
     </script>
+    <style>
+        #imgContainer{
+            width:50vw;
+            height:80vh;
+            position: relative;
+            margin: auto;
+            overflow: hidden;
+        }
+        button{
+            position: absolute;
+        }
+
+        #previous{
+            top:40%;
+            left:10%;
+        }
+        #next{
+            bottom:60%;
+            left:80%;
+        }
+    </style>
 </head>
 <body>
     <button type="button" id="previous">Previous</button>
+    <div id="imgContainer"></div>
     <button type="button" id="next">Next</button>
+
 </body>
 </html>
