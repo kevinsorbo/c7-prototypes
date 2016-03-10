d455640c1a7dbafb0d246cb9fec1ef23c5e4c90e
Korey Lo <koreylo9@gmail.com>
2016-03-09 16:53:49 -0800

    feature set 1

--- mysql_basics/mysql_queries.sql
+++ mysql_basics/mysql_queries.sql
@@ -1,1 +1,5 @@
 Place your queries below
+SELECT * FROM `users` WHERE `username` = 'xshinx';
+UPDATE `users` SET `email` = "cs_shin@gmail.com" WHERE `email`="shin@yahoo.com";
+INSERT INTO users(`username`,`email`,`password`) VALUES ("mclor",'mclor@uci.edu','lovebug');
+DELETE FROM `users` WHERE `username` = "mclor";
