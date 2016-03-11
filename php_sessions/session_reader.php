9f93e5d853137e2dcd810129d3fa0779e3c8aab9
Korey Lo <koreylo9@gmail.com>
2016-03-10 21:55:45 -0800

    php_sessions - Korey Lo

--- php_sessions/session_reader.php
+++ php_sessions/session_reader.php
@@ -1,12 +1,14 @@
-<!-- Session Reader -->
 <?php
     session_start();
-//    foreach($_GET as $inputLabel => $inputValue){
-//        print("Label: $inputLabel; Value: $inputValue <br>");
     header('location: session_setter.php');
-
-    if(!preg_match('^[1-9]$',$_GET['name'])){
-
+    $_SESSION['errors'] = [];
+    if(!preg_match('/^[a-zA-Z]*$/',$_GET['name'])){
+        $_SESSION['errors']['name'] = "Invalid Name Entry: Letters Only";
     }
-// }
+    if(!preg_match('/^[1-9]{1,2}?$/',$_GET['age'])){
+        $_SESSION['errors']['age'] = "Invalid Age Entry: 1-99";
+    }
+    if(!preg_match('/^[a-zA-Z]*$/',$_GET['occupation'])){
+        $_SESSION['errors']['occupation'] = "Invalid Occupation Entry: Letters Only";
+    }
  ?>
--- php_sessions/session_setter.php
+++ php_sessions/session_setter.php
@@ -1,4 +1,3 @@
-<!-- Session Setter -->
 <?php
 session_start();
 ?>
@@ -11,10 +10,16 @@ session_start();
     <meta charset="utf-8">
 </head>
 <body>
+    <!-- empty function used to check if errors[key] is empty and for regex checking done in session_reader
     <form action="session_reader.php" method="get">
-        Name: <input type="text" name='name' value="<?=array_key_exists('name',$_SESSION)? $_SESSION['name']:''?>"><br>
-        Age: <input type="text" name="age" value="<?=array_key_exists('age',$_SESSION)?$_SESSION['age']:''?>"><br>
+        Name: <input type="text" name='name' value="<?=array_key_exists('name',$_SESSION)? $_SESSION['name']:''?>">
+        <?=empty($_SESSION['errors']['name'])?'':$_SESSION['errors']['name']?>
+        <br>
+        Age: <input type="text" name="age" value="<?=array_key_exists('age',$_SESSION)?$_SESSION['age']:''?>">
+        <?=empty($_SESSION['errors']['age'])?'':$_SESSION['errors']['age']?>
+        <br>
         Occupation: <input type="text" name='occupation' value="<?=array_key_exists('occupation',$_SESSION)?$_SESSION['occupation']:''?>">
+        <?=empty($_SESSION['errors']['occupation'])?'':$_SESSION['errors']['occupation']?>
         <input type="submit" value="submit">
     </form>
 </body>
