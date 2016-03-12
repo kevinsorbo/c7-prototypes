3220e3e9b4c7867ef42061393ecca901d5a25575
Korey Lo <koreylo9@gmail.com>
2016-03-11 18:53:18 -0800

    ang_routes Korey Lo

--- ang_routes/script.js
+++ ang_routes/script.js
@@ -1,8 +1,24 @@
 // Create the route module and name it routeApp
-var routeApp = angular.module("routeApp",[]);
+var routeApp = angular.module("routeApp",['ngRoute']);
 
 // Config the routes
 
+routeApp.config(function($routeProvider){
+
+    $routeProvider
+        .when('/home',{
+            templateUrl:'pages/home.html',
+            controller:'homePageCtrl'
+        })
+        .when('/about',{
+            templateUrl:'pages/about.html',
+            controller:'aboutPageCtrl'
+        })
+        .when('/contact',{
+            templateUrl:'pages/contact.html',
+            controller:'contactPageCtrl'
+        })
+});
          // route for the home page
 
         // route for the about page
@@ -14,6 +30,15 @@ var routeApp = angular.module("routeApp",[]);
 
 routeApp.controller("routeCtrl",function($scope){
     $scope.message = "lorem ipsum";
+})
+.controller('homePageCtrl',function($scope){
+    $scope.message = "This is the Home Page";
+})
+.controller('aboutPageCtrl',function($scope){
+    $scope.message = "This is the About Page";
+})
+.controller('contactPageCtrl',function($scope){
+    $scope.message = "This is the Contact Page";
 });
 
 // home page controller
