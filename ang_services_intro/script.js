9245cb114f970e6fd4560befa80acda717705275
Korey Lo <koreylo9@gmail.com>
2016-04-19 01:05:07 -0700

    fixed bug dealing with incorrectly declaring controller

--- ang_services_intro/script.js
+++ ang_services_intro/script.js
@@ -3,8 +3,8 @@
 
 
 
-var app = angular.module('mytunes',[]);
-app.controller = ('tunesController',function($scope,$http,$log){
+var tunes = angular.module('myTunes',[]);
+tunes.controller('tunesController',function($scope,$http,$log){
     var self = this;
     this.constructUrl = function(searchTerm){
         return "https://itunes.apple.com/search?term="+searchTerm+"&callback=JSON_CALLBACK";
