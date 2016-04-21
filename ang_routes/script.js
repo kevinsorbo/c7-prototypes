90aca919353fc3a1e123d46fafbfa4ab22130b9b
Korey Lo <koreylo9@gmail.com>
2016-04-21 09:28:49 -0700

    created app and controller and displayed message based on global var

--- ang_routes/script.js
+++ ang_routes/script.js
@@ -1,4 +1,5 @@
 // Create the route module and name it routeApp
+var route = angular.module('routeApp',[]);
 
 // Config the routes
 
@@ -10,6 +11,15 @@
 
 
 // Create the controllers for the different pages below
+route.controller('routeCtrl',function($scope){
+    $scope.message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+        "Vivamus pulvinar non sapien eget convallis. In et dui nisi. Donec at ornare odio. " +
+        "Ut sed felis at tellus fermentum commodo eu vitae lectus. Nulla posuere laoreet " +
+        "tellus quis faucibus. Integer ultricies nisl ac nisl euismod fermentum. Morbi odio neque, " +
+        "ornare id porttitor nec, molestie quis tortor. Quisque eget luctus lacus. Phasellus " +
+        "vestibulum arcu vitae libero laoreet, at pulvinar dolor condimentum. Praesent ut eros " +
+        "tempor augue vestibulum hendrerit. Duis volutpat eros sit amet luctus tincidunt.";
+});
 
 // home page controller
     // Create a message to display in the view
